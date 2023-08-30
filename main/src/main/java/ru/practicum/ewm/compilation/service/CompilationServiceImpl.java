21a6b78e69229c568934e880194ce9494b0852d0
Victor99999987 <volozhaninvs@xn--90acjidgfhdqiaistf2e9i.xn--p1ai>
2023-08-30 09:04:54 +0700

    fix: style

--- main/src/main/java/ru/practicum/ewm/compilation/service/CompilationServiceImpl.java
+++ main/src/main/java/ru/practicum/ewm/compilation/service/CompilationServiceImpl.java
@@ -17,7 +17,6 @@ import ru.practicum.ewm.compilation.repository.CompilationRepository;
 import ru.practicum.ewm.event.model.Event;
 import ru.practicum.ewm.event.repository.EventRepository;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
